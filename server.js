28425bbc77622583a5ffbdc312ec648d4ac28887
kalvium <sanya.thakur@kalvium.community>
2025-01-16 14:14:36 +0530

    editted server.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 app.get('/ping', (req, res) => {
-  res.send('Pong!');
+  res.send('pong!');
 });
 
 app.listen(PORT, () => {
