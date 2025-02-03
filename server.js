25856d987dbcf58b9e3e76860dcf23fd4261c44f
kalvium <sanya.thakur@kalvium.community>
2025-02-03 14:15:38 +0530

    Connected MongoDB

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const mongoose= require('mongoose');
+require('dotenv').config();
 const app = express();
+app.use(express.json());
 const PORT = 3000;
+const MONGO_URI= process.env.MONGO_URI;
 
-app.get('/ping', (req, res) => {
-  res.send('pong!');
+
+mongoose.connect(MONGO_URI).then(()=>console.log("Connected to database.")).catch((err)=>console.log('Falied: ', err));
+
+
+app.get('/', (req, res) => {
+  const status= mongoose.connection.readyState===1 ? "Connected" : "Not Connected";
+  res.status(200).json({message: "Welcome to WhatTheWoof", database: status});
 });
 
 app.listen(PORT, () => {
